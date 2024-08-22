67f2186f3288670edddfab5bf09758af1fb58454
Halima Ehsan <halimabutt33@gmail.com>
2024-08-22 22:04:31 +0500

    Authentication added

--- app.py
+++ app.py
@@ -1,11 +1,13 @@
-from flask import Flask, render_template,request, redirect
+from flask import Flask, render_template,request, redirect, session
 from flask_sqlalchemy import SQLAlchemy
 from datetime import datetime
+import bcrypt
 
 app = Flask(__name__)
 app.config['SQLALCHEMY_DATABASE_URI'] = "sqlite:///todo.db"
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 db = SQLAlchemy(app)
+app.secret_key = 'secret_key'
 
 class Todo(db.Model):
     sno = db.Column(db.Integer, primary_key = True)
@@ -16,16 +18,40 @@ class Todo(db.Model):
     def __repr__(self) -> str:
         return f"{self.sno} - {self.title}"
 
-@app.route('/', methods=['GET', 'POST'])
+class User(db.Model):
+    id = db.Column(db.Integer, primary_key = True)
+    name = db.Column(db.String(100), nullable = False)
+    email = db.Column(db.String(100), nullable = False, unique = True)
+    password = db.Column(db.String(100), nullable = False)
+
+    def __init__(self, name, email, password):
+        self.name = name
+        self.email = email
+        self.password = bcrypt.hashpw(password.encode('utf-8'), bcrypt.gensalt())
+    
+    def check_password(self, password):
+        return bcrypt.checkpw(password.encode('utf-8'), self.password)
+with app.app_context():
+    db.create_all()
+
+@app.route('/')
+def title_page():
+     return render_template('front.html')
+
+
+@app.route('/home', methods=['GET', 'POST'])
 def hello_world():
-    if request.method== 'POST':
-        title = request.form['title']
-        desc = request.form['desc']
-        todo = Todo(title=title, desc=desc)
-        db.session.add(todo)
-        db.session.commit()
-    allTodo = Todo.query.all()
-    return render_template('index.html', allTodo=allTodo)
+    if 'name' in session:
+        if request.method== 'POST':
+            title = request.form['title']
+            desc = request.form['desc']
+            todo = Todo(title=title, desc=desc)
+            db.session.add(todo)
+            db.session.commit()
+        allTodo = Todo.query.all()
+        return render_template('index.html', allTodo=allTodo)
+    else:
+        return redirect('/login')
 
 @app.route('/about')
 def about():
@@ -47,7 +73,7 @@ def update(sno):
         todo.desc = desc
         db.session.add(todo)
         db.session.commit()
-        return redirect("/")
+        return redirect("/home")
     todo = Todo.query.filter_by(sno=sno).first()
     return render_template('update.html', todo=todo)
 
@@ -56,7 +82,43 @@ def delete(sno):
     todo = Todo.query.filter_by(sno=sno).first()
     db.session.delete(todo)
     db.session.commit()
-    return redirect("/")
+    return redirect("/home")
+
+@app.route('/register', methods=['GET', 'POST'])
+def register():
+    if request.method == 'POST':
+        name = request.form['name']
+        email = request.form['email']
+        password = request.form['password']
+
+        new_user = User(name=name, email=email, password=password)
+        db.session.add(new_user)
+        db.session.commit()
+        return redirect("/login")
+    
+    return render_template('register.html')
+
+@app.route('/login', methods=['GET', 'POST'])
+def login():
+    if request.method == 'POST':
+        email = request.form['email']
+        password = request.form['password']
+
+        user = User.query.filter_by(email=email).first()
+
+        if user and user.check_password(password):
+            session['name'] = user.name
+            session['email'] = user.email
+            return redirect('/home')
+        else:
+            return render_template('login.html', error='Invalid User')
+    return render_template('login.html')
+
+@app.route('/logout', methods=['Get'])
+def logout():
+    session.pop('name', None)
+    session.pop('email', None)
+    return redirect('/login')
 
 if __name__ == "__main__":
     app.run(debug=True)
